d5529c31eb52d0037ffe9ff30409401124f2f05c
Katka Pilátová <tlacencin@gmail.com>
2024-07-07 13:19:34 +0200

    Generate cms project

---
+++ apps/cms-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run cms:serve:development',
+        production: 'nx run cms:serve:production',
+      },
+      ciWebServerCommand: 'nx run cms:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
