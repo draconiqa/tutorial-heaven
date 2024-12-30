25a250a7477a310ed974e25c53f6b45fb7dbaf43
Matěj Chalk <matej.chalk@flowup.cz>
2024-12-30 18:04:20 +0100

    feat(api): create basic graphql server

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,10 @@
-import express from 'express';
+import { createServer } from './app/server';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
-
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+createServer().then(server => {
+  server.listen(port, host, () => {
+    console.log(`[ ready ] http://${host}:${port}/graphql`);
+  });
 });
