a9c59270ba5a08136feb479e50b886a89963003a
Matěj Chalk <matej.chalk@flowup.cz>
2024-12-30 14:41:13 +0100

    revert(cms): use standalone components

--- apps/cms/src/app/app.component.ts
+++ apps/cms/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
+  standalone: true,
   imports: [NxWelcomeComponent, RouterModule],
   selector: 'th-root',
   templateUrl: './app.component.html',
