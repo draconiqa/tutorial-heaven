e364f749826d4a6bf105668bcff255b6cbe52e75
Matěj Chalk <matej.chalk@flowup.cz>
2024-12-30 15:47:41 +0100

    feat(cms): generate ng-zorro setup

---
+++ apps/cms/src/app/icons-provider.ts
@@ -0,0 +1,13 @@
+import {
+  DashboardOutline,
+  FormOutline,
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+} from '@ant-design/icons-angular/icons';
+
+export const icons = [
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+  DashboardOutline,
+  FormOutline,
+];
