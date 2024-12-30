6ee40273071472bd345e77ad895b244c928ab90a
Matěj Chalk <matej.chalk@flowup.cz>
2024-12-30 14:51:59 +0100

    feat(cms): remove nx-welcome component (styles budget error post-update)

--- apps/cms/src/app/app.component.ts
+++ apps/cms/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'th-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
-export class AppComponent {
-  title = 'cms';
-}
+export class AppComponent {}
