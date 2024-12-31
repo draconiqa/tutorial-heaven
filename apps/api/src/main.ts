56b0c9296fdbae13202444f05b3c6b30d3e098f9
Matěj Chalk <matej.chalk@flowup.cz>
2024-12-31 16:25:45 +0100

    fix(api): change default port to not conflict with Next.js

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,7 @@
 import { createServer } from './app/server';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3333;
 
 createServer().then(server => {
   server.listen(port, host, () => {
