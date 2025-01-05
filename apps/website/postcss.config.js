fb50d9f6932992d5c6bcdb0641499d8a0847600e
Matěj Chalk <matej.chalk@flowup.cz>
2025-01-05 10:34:58 +0100

    build: convert from CommonJS to ES Modules

--- apps/website/postcss.config.js
+++ apps/website/postcss.config.js
@@ -1,15 +1,14 @@
-const { join } = require('path');
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 // Note: If you use library-specific PostCSS/Tailwind configuration then you should remove the `postcssConfig` build
 // option from your application's configuration (i.e. project.json).
 //
 // See: https://nx.dev/guides/using-tailwind-css-in-react#step-4:-applying-configuration-to-libraries
 
-module.exports = {
-  plugins: {
-    tailwindcss: {
-      config: join(__dirname, 'tailwind.config.js'),
-    },
-    autoprefixer: {},
+export const plugins = {
+  tailwindcss: {
+    config: join(fileURLToPath(dirname(import.meta.url)), 'tailwind.config.js'),
   },
+  autoprefixer: {},
 };
--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,17 +1,18 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+import { createGlobPatternsForDependencies } from '@nx/react/tailwind';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const projectDir = fileURLToPath(dirname(import.meta.url));
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = [
+  join(
+    projectDir,
+    '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
+  ),
+  ...createGlobPatternsForDependencies(projectDir),
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
