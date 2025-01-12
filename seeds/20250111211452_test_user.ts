298afd216c6780ba54b1bfbf7a5c443d4fb2aedb
Matěj Chalk <matej.chalk@flowup.cz>
2025-01-12 10:37:45 +0100

    ci: create test user before running e2e using kysely seeds

---
+++ seeds/20250111211452_test_user.ts
@@ -0,0 +1,37 @@
+import type { Kysely } from 'kysely';
+import { hash } from './utils/hash';
+
+export async function seed(db: Kysely<any>): Promise<void> {
+  const user = {
+    email: 'john.doe@example.com',
+    displayName: 'John Doe',
+    password: 'password',
+  };
+
+  if (process.env['NODE_ENV'] === 'production') {
+    console.warn(`Skipping test user ${user.email} in production environment`);
+    return;
+  }
+
+  const [existingUser] = await db
+    .selectFrom('users')
+    .where('email', '=', user.email)
+    .limit(1)
+    .select('id')
+    .execute();
+  if (existingUser) {
+    console.warn(
+      `Test user ${user.email} already exists (ID: ${existingUser.id})`,
+    );
+    return;
+  }
+
+  await db
+    .insertInto('users')
+    .values({
+      email: user.email,
+      display_name: user.displayName,
+      password_hash: hash(user.password),
+    })
+    .execute();
+}
